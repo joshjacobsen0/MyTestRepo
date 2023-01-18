75ff6823c94a25a29ab0be52295956565689912e
joshjacobsen0 <joshj@entouch.net>
2023-01-17 19:40:19 -0600

    stuff

--- nxapi-lab.py
+++ nxapi-lab.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import re
 
 switchUser = 'admin'
 switchPassword = 'admin'
@@ -35,3 +36,33 @@ token = auth_response['imdata'][0]['aaaLogin']['attributes']['token']
 cookies = {}
 cookies['APIC-cookie'] = token
 print(cookies)
+
+############## Parse Response ##############
+
+counter = 0
+nei_count = response['ins_api']['outputs']['output']['body']['neigh_count']
+print("neighbor count is " + str(nei_count))
+
+while counter < nei_count:
+    hostname = response['ins_api']['outputs']['output']['body'][
+        'TABLE_cdp_neighbor_brief_info']['ROW_cdp_neighbor_brief_info'][counter]['device_id']
+    local_int = response[
+        'ins_api']['outputs']['output']['body']['TABLE_cdp_neighbor_brief_info']['ROW_cdp_neighbor_brief_info'][counter]['intf_id']
+    remote_int = response[
+        'ins_api']['outputs']['output']['body']['TABLE_cdp_neighbor_brief_info']['ROW_cdp_neighbor_brief_info'][counter]['port_id']
+
+    body = {'l1PhysIf': {
+        'attributes': {
+            'descr': 'Connected to '+hostname+' Remote int is '+remote_int
+        }
+    }}
+
+    counter += 1
+    if local_int != 'mgmt0':
+        int_name = str.lower(str(local_int[:3]))
+        int_num = re.search(r'[1-9]/[1-9]*', local_int)
+        int_url = 'https://192.168.1.68/api/mo/sys/intf/phys-['+int_name+str(
+            int_num.group(0))+'].json'
+        post_response = requests.post(int_url, data=json.dumps(
+            body), headers=myHeader, cookies=cookies, verify=False).json()
+        print(post_response)
